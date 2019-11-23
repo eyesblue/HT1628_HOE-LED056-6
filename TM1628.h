1aabfe1a5d599ae5e5920c335a13fbfbd7f52a4b
eyesblue <eyesblue@eyes-blue.com>
2019-11-23 19:48:06 +0800

    Create TM1628.h

---
+++ TM1628.h
@@ -0,0 +1,30 @@
+#ifndef TM1628_h
+#define TM1628_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class TM1628
+{
+  public:
+	// init
+	TM1628(byte _dio_pin, byte _clk_pin, byte _stb_pin);
+	
+	void begin(boolean active, byte intensity);
+	void clear();
+
+	void setNum(uint8_t index, uint8_t num, bool hasDot);
+
+  void sendData(byte addr, byte data);
+  void sendCommand(byte data);
+  void send(byte data);
+    
+  byte _dio_pin;
+  byte _clk_pin;
+  byte _stb_pin;
+};
+
+#endif
